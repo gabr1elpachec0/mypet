32be231ba797919ba0c0196f915a92c0a0de327d
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2024-07-30 21:06:19 -0300

    feat: add support for update pet route

--- server/src/http/pets/pets-routes.ts
+++ server/src/http/pets/pets-routes.ts
@@ -75,4 +75,47 @@ export async function petsRoutes(app: FastifyInstance) {
       console.error(e)
     }
   })
+
+  app.put('/pets/:petId', async (request, reply) => {
+    const { petId } = z.object({
+      petId: z.string().uuid()
+    }).parse(request.params)
+
+    const { name, birthDate, breed, size, gender } = z.object({
+      name: z.string(),
+      birthDate: z.string(),
+      breed: z.string(),
+      size: z.string(),
+      gender: z.string()
+    }).parse(request.body)
+
+    try {
+      const pet = await prisma.pet.findUnique({
+        where: {
+          id: petId
+        }
+      })
+
+      if (!pet) {
+        return reply.status(404).send({ error: 'Pet not found.' })
+      }
+
+      await prisma.pet.update({
+        where: {
+          id: petId
+        },
+        data: {
+          name,
+          birthDate,
+          breed,
+          size,
+          gender
+        }
+      })
+
+      return reply.status(200).send({ message: `${pet.name} was updated succesfully.` })
+    } catch(e) {
+      console.error(e)
+    }
+  })
 }
