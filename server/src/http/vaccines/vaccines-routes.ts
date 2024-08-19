f70353b3a46d26d8307b29fd01926692892c3924
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2024-08-19 17:51:39 -0300

    feat: add support for update/delete vaccines routes

--- server/src/http/vaccines/vaccines-routes.ts
+++ server/src/http/vaccines/vaccines-routes.ts
@@ -38,7 +38,7 @@ export async function vaccinesRoutes(app: FastifyInstance) {
     app.post('/pets/:petId/vaccines', async (request, reply) => {
         const { petId } = z.object({
             petId: z.string().uuid()
-        }).parse(request.params) 
+        }).parse(request.params)
 
         const {
             vaccineName,
@@ -74,6 +74,80 @@ export async function vaccinesRoutes(app: FastifyInstance) {
             })
 
             return reply.status(201).send({ petName: pet.name, message: 'Vaccine registered successfully.' })
+        } catch (error) {
+            console.error(error)
+        }
+    })
+
+    app.put('/vaccines/:vaccineId', async (request, reply) => {
+        const { vaccineId } = z.object({
+            vaccineId: z.string().uuid()
+        }).parse(request.params)
+
+        const {
+            vaccineName,
+            vetName,
+            vaccineDate,
+            vaccineRepeatDate
+        } = z.object({
+            vaccineName: z.string(),
+            vetName: z.string(),
+            vaccineDate: z.string(),
+            vaccineRepeatDate: z.string()
+        }).parse(request.body)
+
+        try {
+            const vaccine = await prisma.vaccines.findUnique({
+                where: {
+                    id: vaccineId
+                }
+            })
+
+            if (!vaccine) {
+                return reply.status(404).send({ message: 'Vaccine register not found.' })
+            }
+
+            await prisma.vaccines.update({
+                where: {
+                    id: vaccineId
+                },
+                data: {
+                    vaccineName,
+                    vetName,
+                    vaccineDate,
+                    vaccineRepeatDate
+                }
+            })
+
+            return reply.status(200).send({ message: 'Vaccine register updated successfully.', vaccine })
+        } catch (error) {
+            console.error(error)
+        }
+    })
+
+    app.delete('/vaccines/:vaccineId', async (request, reply) => {
+        const { vaccineId } = z.object({
+            vaccineId: z.string().uuid()
+        }).parse(request.params)
+
+        try {
+            const vaccine = await prisma.vaccines.findUnique({
+                where: {
+                    id: vaccineId
+                }
+            })
+
+            if (!vaccine) {
+                return reply.status(404).send({ message: 'Vaccine register not found.' })
+            }
+
+            await prisma.vaccines.delete({
+                where: {
+                    id: vaccineId
+                }
+            })
+
+            return reply.status(200).send({ message: 'Vaccine register deleted successfully.' })
         } catch (error) {
             console.error(error)
         }
